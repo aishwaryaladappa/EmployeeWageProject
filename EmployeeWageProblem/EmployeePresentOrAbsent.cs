9c27f020f0d4840ee8ba3fad9f2d520c3ccaba3f
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-08-08 17:10:32 +0530

    added program.cs

---
+++ EmployeeWageProblem/EmployeePresentOrAbsent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EmployeeWageProblem
+{
+    public static class EmployeePresentOrAbsent
+    {
+        public static void Attendance()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(2);
+            if (empCheck == IS_FULL_TIME)
+
+            {
+                Console.WriteLine("employee is present");
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
+            
+        }
+    }
+}
---
+++ EmployeeWageProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace EmployeeWageProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+
+            EmployeePresentOrAbsent.Attendance();
+            EmployeeWage.Wage();
+
+
+            Parttime.EmpParttime();
+            Switchcase.EmpWageSwitch();
+            ForTwentyDays.EmpWageSwitch();
+            TotalWorkingHourPerMonth.TotalWorkingHrs();
+        }
+    }
+}
